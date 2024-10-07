869b6ba638d532e8904ac0f3006c9d9186b1ef9c
Cesar <50472267+EmilCes@users.noreply.github.com>
2024-10-06 23:17:51 -0600

    Add teacher service

--- src/teachers/teachers.controller.ts
+++ src/teachers/teachers.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Put } from '@nestjs/common';
 import { TeachersService } from './teachers.service';
 import { CreateTeacherDto } from './dto/create-teacher.dto';
 import { UpdateTeacherDto } from './dto/update-teacher.dto';
@@ -13,8 +13,18 @@ export class TeachersController {
   }
 
   @Get()
-  findAll() {
-    return this.teachersService.findAll();
+  async findAll(
+    @Query('page') page = 1,
+    @Query('limit') limit = 10,
+    @Query('searchTerm') searchTerm?: string
+  ) {
+    const limitNumber = Math.max(1, Number(limit));
+    const pageNumber = Math.max(1, Number(page));
+
+    const { teachers, total } = await this.teachersService.findAll(pageNumber, limitNumber, searchTerm);
+    const lastPage = Math.ceil(total / limitNumber);
+
+    return { teachers, total, lastPage };
   }
 
   @Get(':id')
@@ -22,7 +32,7 @@ export class TeachersController {
     return this.teachersService.findOne(id);
   }
 
-  @Patch(':id')
+  @Put(':id')
   update(@Param('id') id: string, @Body() updateTeacherDto: UpdateTeacherDto) {
     return this.teachersService.update(id, updateTeacherDto);
   }
--- src/teachers/teachers.service.ts
+++ src/teachers/teachers.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTeacherDto } from './dto/create-teacher.dto';
 import { UpdateTeacherDto } from './dto/update-teacher.dto';
 import { PrismaService } from 'src/prisma.service';
@@ -6,20 +6,60 @@ import { PrismaService } from 'src/prisma.service';
 @Injectable()
 export class TeachersService {
 
-  constructor (
+  constructor(
     private prisma: PrismaService
-  ) {}
+  ) { }
 
   async create(createTeacherDto: CreateTeacherDto) {
-    const newTeacher = await this.prisma.teacher.create({ data: {
-      ...createTeacherDto
-    }});
+
+    const existingTeacher = await this.prisma.teacher.findUnique({
+      where: {
+        personalNumber: createTeacherDto.personalNumber
+      }
+    });
+
+    if (existingTeacher) {
+      throw new ConflictException(`El numero de personal ya esta registrado.`);
+    }
+
+    const newTeacher = await this.prisma.teacher.create({
+      data: {
+        ...createTeacherDto
+      }
+    });
 
     return newTeacher;
   }
 
-  async findAll() {
-    return await this.prisma.teacher.findMany();
+  async findAll(page: number, limit: number, searchTerm?: string) {
+
+    const where = searchTerm
+      ? {
+        OR: [
+          { name: {startsWith: searchTerm } }
+        ]
+      }
+      : {};
+
+    const total = await this.prisma.teacher.count({
+      where
+    });
+
+    const teachersbD = await this.prisma.teacher.findMany({
+      where,
+      skip: (page - 1) * limit,
+      take: limit
+    });
+
+    const teachers = teachersbD.map(teacher => {
+      return {
+        ...teacher,
+        birthdate: teacher.birthdate.toISOString().split('T')[0],
+        uvAdmissionDate: teacher.uvAdmissionDate.toISOString().split('T')[0]
+      }
+    });
+
+    return { teachers, total };
   }
 
   async findOne(id: string) {
@@ -31,8 +71,33 @@ export class TeachersService {
     return teacherFound;
   }
 
-  update(id: string, updateTeacherDto: UpdateTeacherDto) {
-    return `This action updates a #${id} teacher`;
+  async update(id: string, updateTeacherDto: UpdateTeacherDto) {
+      const existingTeacher = await this.prisma.teacher.findUnique({
+        where: {
+          idTeacher: id
+        }
+      });
+
+      if (!existingTeacher) {
+        throw new NotFoundException(`Profesor no encontrado.`)
+      }
+
+      const otherTeacher = await this.prisma.teacher.findUnique({
+        where: {
+          personalNumber: updateTeacherDto.personalNumber
+        }
+      });
+
+      if (otherTeacher && otherTeacher.idTeacher !== existingTeacher.idTeacher) {
+        throw new ConflictException(`El numero de personal ya esta registrado.`);
+      }
+
+      const updatedTeacher = await this.prisma.teacher.update({
+        where: { idTeacher: id },
+        data: updateTeacherDto
+      });
+
+      return updatedTeacher;
   }
 
   async remove(id: string) {
