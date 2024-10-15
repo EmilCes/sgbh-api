ba471a323028384240742d940ce6a02f1b111efd
Cesar <50472267+EmilCes@users.noreply.github.com>
2024-10-15 07:15:44 -0600

    Add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { ClassroomsModule } from './classrooms/classrooms.module';
 import { TeachersModule } from './teachers/teachers.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ClassroomsModule, TeachersModule, AuthModule],
+  imports: [
+    ClassroomsModule, 
+    TeachersModule, 
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    })
+  ],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { JwtAuthGuard } from './jwt-auth.guard';
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { 
-        expiresIn: '3600s' 
+        expiresIn: process.env.JWT_EXPIRATION
       }
     })
   ],
