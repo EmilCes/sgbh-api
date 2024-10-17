f463ab7dc10fe091b9dabe43fe2c9f6da835da68
Cesar <50472267+EmilCes@users.noreply.github.com>
2024-10-16 22:30:39 -0600

    Add CORS config

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.setGlobalPrefix('api');
+
   app.enableCors({
-    origin: '*'
+    origin: true,
+    methods: 'GET, PUT, POST, DELETE',
+    credentials: true
   });
   await app.listen(3002);
 }
